123d42af93970f8f54e5c74932fef1188362b28f
Nourhan Adel <143334844+Nora-Shalaby@users.noreply.github.com>
2026-06-04 22:44:26 +0300

    solve cors problem

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ import env from "dotenv";
 import express from "express";
 import connectDB from "./config/DB.js";
 import authRouter from "./src/routes/Auth.routes.js";
+import cors from "cors";
 
 env.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+
 app.use("/api/auth", authRouter);
+
 const PORT = process.env.PORT || 5000;
-const JWT_SECRET = process.env.JWT_SECRET;
 
 const startServer = async () => {
   await connectDB();
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
