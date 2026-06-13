a7581e8f108ea8e90f6ab010a497856f2dfe5d95
Nourhan Adel <143334844+Nora-Shalaby@users.noreply.github.com>
2026-06-13 15:29:20 +0300

    add logout route ant auth.route and at postman

--- src/modules/Auth/auth.route.js
+++ src/modules/Auth/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login, register } from "./auth.controller.js";
+import { login, register, logout } from "./auth.controller.js";
 import { verifyRole } from "../../Middleware/Auth.Middleware.js";
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
